f02f13dd183dd67e65f4ad484f9a2a7012275ee3
SaidCarrera <jose.carrera.montesdeoca@udla.edu.ec>
2024-11-09 15:17:14 -0500

    Atualización

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
-  correo: { type: String, required: true, unique: true },
+  correo: { type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,25 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Importa el modelo de usuario
+const User = require('../models/User');
 
-// Ruta para obtener la lista de usuarios
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    console.error('Error al obtener los usuarios:', error);
-    res.status(500).json({ error: 'Error al obtener los usuarios' });
-  }
-});
-
-// Ruta para crear un nuevo usuario
+// Endpoint para crear un usuario
 router.post('/', async (req, res) => {
+  
+  const { nombre, correo } = req.body;
   try {
-    const { nombre, correo } = req.body;
     const newUser = new User({ nombre, correo });
     await newUser.save();
-    res.status(201).json(newUser); // Responde con el usuario creado
+    res.status(201).json(newUser);
   } catch (error) {
     console.error('Error al crear el usuario:', error);
     res.status(500).json({ error: 'Error al crear el usuario' });
